780a6c67e5b26cbeb1ea06a49a41851b6ff11be6
kaitlynellis <122844255+kaitlynellis@users.noreply.github.com>
2026-03-18 16:43:58 -0400

    Update entity.h

--- src/game/entity.h
+++ src/game/entity.h
@@ -27,8 +27,8 @@ typedef struct{
 
     char facing; //this will be used to determine which direction they are facing
 
-    int hitbox_offset_x;
-    int hitbox_offset_y;
+    int hitbox_x;
+    int hitbox_y;
     int hitbox_w;
     int hitbox_h;
 
